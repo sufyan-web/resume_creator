0525df85f05f6280ef03ddcaede6eb0ab481ba1b
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-09 10:16:20 +0500

    basc things added

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// next.config.mjs
+export default {
+    images: {
+        domains: ['cdn-icons-png.flaticon.com' , 'cdn6.aptoide.com', 'play-lh.googleusercontent.com'],
+    },
+  };
